49e85422cfcf59472fb82e2e15a56e5e2129643a
Anna Xu <skateboardingalice@gmail.com>
2017-05-10 17:30:49 -0500

    yay is working
    still need to comment and fix the alarm words

--- app/src/main/java/com/example/axu1/richarddawkinsalarmclock/MainActivity.java
+++ app/src/main/java/com/example/axu1/richarddawkinsalarmclock/MainActivity.java
@@ -25,17 +25,20 @@ public class MainActivity extends AppCompatActivity {
     private PendingIntent pending_intent;
 
     private TimePicker alarmTimePicker;
-    private static MainActivity inst;
     private TextView alarmTextView;
 
     private AlarmReceiver alarm;
-    private Context context;
 
+
+    MainActivity inst;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         this.context = this;
 
@@ -62,22 +65,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 calendar.add(Calendar.SECOND, 3);
-                setAlarmText("You clicked a button");
+                //setAlarmText("You clicked a button");
+
+                final int hour = alarmTimePicker.getCurrentHour();
+                final int minute = alarmTimePicker.getCurrentMinute();;
+
+                Log.e("MyActivity", "In the receiver with " + hour + " and " + minute);
+                setAlarmText("You clicked a " + hour + " and " + minute);
 
 
-                //calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
-                //calendar.set(Calendar.MINUTE, alarmTimePicker.getMinute());
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
 
-                //final int hour = alarmTimePicker.getHour();
-                //final int minute = alarmTimePicker.getMinute();;
+                myIntent.putExtra("extra", "yes");
+                pending_intent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
 
 
-                //myIntent.putExtra("extra", "yes");
-                //pending_intent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                // now you should change the set Alarm text so it says something nice
 
-                //alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
 
-                //setAlarmText("Alarm set to " + hour + ":" + minute);
+                setAlarmText("Alarm set to " + hour + ":" + minute);
                 //Toast.makeText(getApplicationContext(), "You set the alarm", Toast.LENGTH_SHORT).show();
             }
 
@@ -96,12 +105,12 @@ public class MainActivity extends AppCompatActivity {
                 int random_number = r.nextInt(max - min + 1) + min;
                 Log.e("random number is ", String.valueOf(random_number));
 
+                myIntent.putExtra("extra", "no");
+                sendBroadcast(myIntent);
 
-                //myIntent.putExtra("extra", "no");
-                //sendBroadcast(myIntent);
-
-                //alarmManager.cancel(pending_intent);
+                alarmManager.cancel(pending_intent);
                 setAlarmText("Alarm canceled");
+                //setAlarmText("You clicked a " + " canceled");
             }
         });
 
